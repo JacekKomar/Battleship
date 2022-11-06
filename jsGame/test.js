4fdaac1f038076271b465c341df828c1e03292f0
JacekKomar <jacek.komar2000@gmail.com>
2022-11-06 14:08:50 +0100

    Fixed bugs in js code

--- jsGame/test.js
+++ jsGame/test.js
@@ -71,6 +71,24 @@ var view = {
         return true;
       }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   };
 
 
@@ -171,52 +189,67 @@ var view = {
 
 
 
-  generateShip: function() {
-    var direction = Math.floor(Math.random() * 2);
-  var row, col;
-  if(direction ===1){
-    row = Math.floor(Math.random() * this.boardSize);
-  col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
 
-  }else {
-   row = Math.floor(Math.random() * this.boardSize - this.shipLength);
-    col = Math.floor(Math.random() * (this.boardSize));
-  }
-  
-  }
-  var newShipLocations = [];
-  for (var i = 0; i < this.shipLength; i++){
-    if (direction === 1){
-        newShipLocations.push(row + "" +(col + i));
-  
-    } else {
-newShipLocations.push((row + i) + "" + col);
 
+
+generateShipLocations: function() {
+    var locations; 
+    for (var i = 0; i < this.numShips; i++) {
+        do
+        {
+    locations = this.generateShip();
+    
+    
+        } while (this.collision(locations));
+        this.ships[i].locations = locations;
+    } 
+    
+    },
+    
+    
+      generateShip: function() {
+        var direction = Math.floor(Math.random() * 2);
+      var row, col;
+      if(direction ===1){
+        row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+    
+      }else {
+       row = Math.floor(Math.random() * this.boardSize - this.shipLength);
+        col = Math.floor(Math.random() * (this.boardSize));
+      }
+      
+      }
+      var newShipLocations = [];
+      for (var i = 0; i < this.shipLength; i++){
+        if (direction === 1){
+            newShipLocations.push(row + "" +(col + i));
+      
+        } else {
+    newShipLocations.push((row + i) + "" + col);
+    
     }
-}
-
-return newShipLocations
-},
- 
-
-
-
-  collision: function(locations) {
-    for(var i = 0; i < this.numShips; i++){
-        var ship = model.ships[i];
-        for (var j = 0; j <locations.length; j++) {
-            if (ship.locations.indexOf(locations[j]) >=0) {
-                return true;
+    }
+    
+    return newShipLocations},
+    
+     
+    
+    
+    
+      collision: function(locations) {
+        for(var i = 0; i < this.numShips; i++){
+            var ship = model.ships[i];
+            for (var j = 0; j <locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >=0) {
+                    return true;
+                }
             }
+    
         }
-
-    }
-    return false
-  }
-
-
-
-
+        return false
+      }
+    
 
  
 
