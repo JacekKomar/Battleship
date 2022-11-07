d6378125c770e1f3390d0d55b5e965e766c8783d
JacekKomar <jacek.komar2000@gmail.com>
2022-11-07 14:31:12 +0100

    Add more diversity of photos

--- jsGame/photosChange.js
+++ jsGame/photosChange.js
@@ -1,79 +1,47 @@
 let image = document.getElementById("image");
 let images = [
-  "images/propaganda/1.jpg",
   "images/propaganda/2.jpg",
-  "images/propaganda/3.jpg",
-  "images/propaganda/4.jpg",
   "images/propaganda/5.jpg",
-  "images/propaganda/6.jpg",
-  "images/propaganda/7.jpg",
   "images/propaganda/8.jpg",
-  "images/propaganda/9.jpg",
-  "images/propaganda/10.jpg",
-  "images/propaganda/11.jpg",
   "images/propaganda/12.jpg",
 ];
 setInterval(function () {
-  let random = Math.floor(Math.random() * 12);
+  let random = Math.floor(Math.random() * 4);
   image.src = images[random];
 }, 4000);
 
 let imageTwo = document.getElementById("imageTwo");
 let imagesTwo = [
   "images/propaganda/1.jpg",
-  "images/propaganda/2.jpg",
-  "images/propaganda/3.jpg",
-  "images/propaganda/4.jpg",
-  "images/propaganda/5.jpg",
   "images/propaganda/6.jpg",
   "images/propaganda/7.jpg",
-  "images/propaganda/8.jpg",
-  "images/propaganda/9.jpg",
   "images/propaganda/10.jpg",
-  "images/propaganda/11.jpg",
-  "images/propaganda/12.jpg",
 ];
 setInterval(function () {
-  let random = Math.floor(Math.random() * 12);
+  let random = Math.floor(Math.random() * 4);
   imageTwo.src = imagesTwo[random];
 }, 4000);
 
 let imageThree = document.getElementById("imageThree");
 let imagesThree = [
-  "images/propaganda/1.jpg",
-  "images/propaganda/2.jpg",
-  "images/propaganda/3.jpg",
-  "images/propaganda/4.jpg",
-  "images/propaganda/5.jpg",
-  "images/propaganda/6.jpg",
-  "images/propaganda/7.jpg",
-  "images/propaganda/8.jpg",
-  "images/propaganda/9.jpg",
-  "images/propaganda/10.jpg",
-  "images/propaganda/11.jpg",
-  "images/propaganda/12.jpg",
+  "images/propaganda/13.jpg",
+  "images/propaganda/14.jpg",
+  "images/propaganda/15.jpg",
+  "images/propaganda/16.jpg",
 ];
 setInterval(function () {
-  let random = Math.floor(Math.random() * 12);
+  let random = Math.floor(Math.random() * 4);
   imageThree.src = imagesThree[random];
 }, 4000);
 
 let imageFour = document.getElementById("imageFour");
 let imagesFour = [
-  "images/propaganda/1.jpg",
-  "images/propaganda/2.jpg",
   "images/propaganda/3.jpg",
   "images/propaganda/4.jpg",
-  "images/propaganda/5.jpg",
-  "images/propaganda/6.jpg",
-  "images/propaganda/7.jpg",
-  "images/propaganda/8.jpg",
   "images/propaganda/9.jpg",
-  "images/propaganda/10.jpg",
   "images/propaganda/11.jpg",
-  "images/propaganda/12.jpg",
 ];
 setInterval(function () {
-  let random = Math.floor(Math.random() * 12);
+  let random = Math.floor(Math.random() * 4);
   imageFour.src = imagesFour[random];
 }, 4000);
