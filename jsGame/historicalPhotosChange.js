0c713480a383e94d84a9096af1bf856db03ad837
JacekKomar <jacek.komar2000@gmail.com>
2022-11-09 19:38:18 +0100

    Fixed game rules page

--- jsGame/historicalPhotosChange.js
+++ jsGame/historicalPhotosChange.js
@@ -3,6 +3,7 @@ let historicalsPhotoOne = [
   "images/historicalPhotosInGameRules/4.jpg",
   "images/historicalPhotosInGameRules/1.jpg",
   "images/historicalPhotosInGameRules/2.jpg",
+  "images/historicalPhotosInGameRules/12.jpg",
 ];
 setInterval(function () {
   let random = Math.floor(Math.random() * 3);
@@ -14,8 +15,21 @@ let historicalsPhotoTwo = [
   "images/historicalPhotosInGameRules/6.jpg",
   "images/historicalPhotosInGameRules/3.jpg",
   "images/historicalPhotosInGameRules/5.jpg",
+  "images/historicalPhotosInGameRules/11.jpg",
 ];
 setInterval(function () {
   let random = Math.floor(Math.random() * 3);
   historicalPhotoTwo.src = historicalsPhotoTwo[random];
+}, 4000);
+
+let historicalPhotoThree = document.getElementById("historicalPhotoThree");
+let historicalsPhotoThree = [
+  "images/historicalPhotosInGameRules/7.jpg",
+  "images/historicalPhotosInGameRules/8.jpg",
+  "images/historicalPhotosInGameRules/9.jpg",
+  "images/historicalPhotosInGameRules/10.jpg",
+];
+setInterval(function () {
+  let random = Math.floor(Math.random() * 3);
+  historicalPhotoThree.src = historicalsPhotoThree[random];
 }, 4000);
