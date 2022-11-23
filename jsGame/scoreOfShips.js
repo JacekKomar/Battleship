c5481a6b1a1d88c76384ddf1c9735c8a79b622a6
JacekKomar <jacek.komar2000@gmail.com>
2022-11-23 15:41:59 +0100

    Deleted unnecesary files

--- jsGame/scoreOfShips.js
+++
@@ -1,8 +0,0 @@
-function incrementButton() {
-  var element = document.getElementById("incrementText");
-  var value = element.innerHTML;
-
-  --value;
-  console.log(value);
-  document.getElementById("incrementText").innerHTML = value;
-}
