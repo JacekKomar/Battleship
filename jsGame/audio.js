9a0f692c8d3c519f2e1ddeade7adf3e8fa0a694a
JacekKomar <jacek.komar2000@gmail.com>
2022-11-24 19:20:05 +0100

    Test of sounds

--- jsGame/audio.js
+++ jsGame/audio.js
@@ -1,6 +1,6 @@
 export const SOUND_HIT = "../sounds/shot.mp3";
 export const SOUND_MISS = "../sounds/Miss.mp3";
-export const SOUND_SUNK = "../sounds/DestroyOfShip.mp3";
+export const SOUND_SUNK = "../sounds/hit.mp3";
 
 export function playSound(soundName) {
   const audio = new Audio(soundName);
